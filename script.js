bf8aa1333a0e321dea1ef9f334de8f896ce494b8
Paul Christianson <pacdeveloping@gmail.com>
2024-05-21 23:13:30 -0400

    Got calculator buttons working and most operations work, however addition just concatenates digits instead

--- script.js
+++ script.js
@@ -1,6 +1,53 @@
-let operandOne = null;
-let operandTwo = null;
-let operatorFunc = null;
+let calcState = {
+    one: null,
+    operator: null,
+    two: null,
+    display: document.getElementById('display'),
+    reset: function() {
+        this.one = null;
+        this.operator = null;
+        this.two = null;
+    },
+    nextFrame: function(press) {
+        if (this.one == null){
+            this.one = press;
+        }
+        else if (this.one != null && this.operator == null){
+            this.operator = press;
+        } 
+        else if (this.one != null && this.operator != null && this.two == null) {
+            this.two = press;
+        }
+
+        if (press == "="){
+            console.log(this.one,this.two,this.operator)
+            this.display.textContent = operate(this.one,this.two,buttonOperator(this.operator))
+            calcState.reset()
+        }
+        if (press == "Clear") {
+            this.display.textContent = 0;
+            calcState.reset()
+            }
+    },
+};
+
+function buttonOperator (str) {
+    if (str == "÷"){
+        return divide
+    }
+    if (str == "×"){
+        return multiply
+    }
+    if (str == "-"){
+        return subtract
+    }
+    if (str == "+"){
+        return add
+    }
+    else {
+        return null
+    }
+}
 
 function add (a,b) {
     return a + b;
@@ -18,3 +65,13 @@ function operate (a,b,func) {
     return func(a,b)
 }
 
+// Get all buttons on the page
+const buttons = document.querySelectorAll('button');
+
+// Add a click listener to each button
+buttons.forEach((button) => {
+    button.addEventListener('click', (event) => {
+        buttonText = event.target.textContent;
+        calcState.nextFrame(buttonText)
+    })
+})
